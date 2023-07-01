1d5cb773f202e5c2ec6222ef6283bd5b5596a286
Your Name <you@example.com>
2023-07-01 22:46:34 +0200

    Added Object Pool

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
@@ -6,6 +8,8 @@ public class GameManager : Singleton<GameManager>
 
     public GameObject prefab;
 
+    private ProjectilePool pool;
+
     private void Start()
     {
         Instantiate(prefab);
@@ -13,5 +17,33 @@ public class GameManager : Singleton<GameManager>
         player = Player.Instance;
 
         print(player.health);
+
+        pool = (ProjectilePool) ProjectilePool.Instance;
+
+        pool.PoolObjects(4);
+        
+        StartCoroutine(Demo());
+    }
+
+    private IEnumerator Demo()
+    {
+        List<Projectile> projectileList = new List<Projectile>();
+        Projectile projectile;
+
+        for(int i = 0; i != 7; ++i)
+        {
+            projectile = pool.GetPooledObject();
+            projectileList.Add(projectile);
+            projectile.Randomize();
+            projectile.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+        for (int i = 0; i != 4; ++i)
+        {
+            pool.ReturnObjectToPool(projectileList[i]);
+
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
