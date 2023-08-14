fe9c4f1ac71eadcf99b3e70b6d7bf733a5dcd294
Your Name <you@example.com>
2023-08-14 20:45:00 +0200

    Populating the scene & Cleaning testing stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,27 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+*
+* This class will set up the scene and initialize objects
+*
+* This class inherits from Singleton so any other script can access it easily through GameManager.Instance
+*
+*/
+
 public class GameManager : Singleton<GameManager>
 {
     private MatchablePool pool;
     private MatchableGrid grid;
 
-    [SerializeField] private Vector2Int dimensions;
+    // the dimension of the matchable grid, set in the inspector
+    [SerializeField] private Vector2Int dimensions = Vector2Int.one;
+
+    // a UI Text object for displaying the contents of the grid data
+    // for testing and debugging purposes only
     [SerializeField] private Text gridOutput;
 
     private void Start()
     {
+        // get references to other important game objects
         pool = (MatchablePool) MatchablePool.Instance;
         grid = (MatchableGrid) MatchableGrid.Instance;
 
-        pool.PoolObjects(10);
+        StartCoroutine(Setup());
+    }
+    private IEnumerator Setup()
+    {
+        // Put loading screen here in the future
 
+
+        // pool the matchables
+        pool.PoolObjects(dimensions.x * dimensions.y * 2);
+
+        // create the grid
         grid.InitializeGrid(dimensions);
 
-        StartCoroutine(Demo());
-    }
-    private IEnumerator Demo()
-    {
+        yield return null;
+
+        StartCoroutine(grid.PopulateGrid());
+
+        /*
+
         gridOutput.text = grid.ToString();
         yield return new WaitForSeconds(2);
 
@@ -52,5 +76,6 @@ public class GameManager : Singleton<GameManager>
 
         pool.ReturnObjectToPool(m1);
         pool.ReturnObjectToPool(m2);
+        */
     }
 }
